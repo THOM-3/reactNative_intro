29a5fe822d7f5b5f2a09f43146a0dfa6c07befb5
THOM-3 <juaum211@gmail.com>
2023-08-15 21:40:08 -0300

    123498

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native'; 
+import 'react-native-gesture-handler';
 
 import Constants from 'expo-constants';
 
@@ -12,7 +13,7 @@ import Exemplo_006 from './components/exemplo_006';
 import Exemplo_007 from './components/exemplo_007'; 
 import Exemplo_008 from './components/exemplo_008';
 
-import Atividade_001 from './components/atividade_001'; 
+import Atividade_001 from './components/atividades/atividade_001'; 
 import Atividade_002 from './components/atividade_002'; 
 import Atividade_003 from './components/atividade_003'; 
 import Atividade_004 from './components/atividade_004'; 
@@ -27,7 +28,7 @@ const corApp = '#fff';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo_008 />
+      <Velha />
       <StatusBar style={corApp==='#000' ? 'light' : 'dark'} />
     </View>
   );
